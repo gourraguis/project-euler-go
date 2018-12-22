8f8f2ce93ac02c32e5c1ad6b1ec2c144716f35b6
gourraguis <gourragui.success@gmail.com>
2018-12-22 15:28:55 +0100

    initial commit

---
+++ p002.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fubonacci() func() int {
+	x, y := 0, 1
+	return func() int {
+		res := y + x
+		x = y
+		y = res
+		return res
+	}
+}
+
+func main() {
+	// Problem: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+	res := 0
+	nextFubonacciNumber := fubonacci()
+	for curr := 0; curr < 4000000 ; curr = nextFubonacciNumber() {
+		if curr % 2 == 0 {
+			res += curr
+		}
+	}
+	fmt.Println(res)
+}
---
+++ p003.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func largestPrimeFactor(N int64) int64 {
+	maxPrime := math.Sqrt(float64(N))
+	for i := int64(maxPrime); i > 1; i-- {
+		if big.NewInt(i).ProbablyPrime(20) && N % i == 0 {
+			return i
+		}
+	}
+	return N
+}
+
+func main() {
+	// Problem What is the largest prime factor of the number 600851475143 ?
+	fmt.Println(largestPrimeFactor(600851475143))
+}
